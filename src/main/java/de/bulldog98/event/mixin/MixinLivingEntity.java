8b66969ae56ed6e228f8ff04cb1285f06dab01db
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2020-12-26 19:18:04 +0100

    Fix mixin not working with other mods

--- src/main/java/de/bulldog98/event/mixin/MixinLivingEntity.java
+++ src/main/java/de/bulldog98/event/mixin/MixinLivingEntity.java
@@ -8,10 +8,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public class MixinLivingEntity {
-    @Inject(method = "onKilledBy", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "onKilledBy", at = @At(value = "HEAD", target =
+            "Lnet/minecraft/entity/Entity;onKilledOther(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LivingEntity;)V",
+            shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILEXCEPTION, cancellable = true)
     public void onInteractKilledBy(@Nullable LivingEntity adversary, CallbackInfo info) {
         LivingEntity killedEntity = (LivingEntity) (Object) this;
         ActionResult result = KilledByCallback.EVENT.invoker().interact(killedEntity, adversary);
