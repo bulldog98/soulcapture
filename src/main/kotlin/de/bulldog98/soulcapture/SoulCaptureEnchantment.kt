5b62e35dbb7190b2fd5cadf47e1afa768cf316b9
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2021-01-01 17:44:20 +0100

    Used the new api provided by fabric-api to implement the enchantment effect

--- src/main/kotlin/de/bulldog98/soulcapture/SoulCaptureEnchantment.kt
+++ src/main/kotlin/de/bulldog98/soulcapture/SoulCaptureEnchantment.kt
@@ -1,8 +1,17 @@
 package de.bulldog98.soulcapture
 
 import net.minecraft.enchantment.Enchantment
+import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.enchantment.EnchantmentTarget
+import net.minecraft.entity.Entity
 import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.SpawnEggItem
+import net.minecraft.server.world.ServerWorld
+import java.util.Random
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
 class SoulCaptureEnchantment : Enchantment(
     Rarity.VERY_RARE,
@@ -11,4 +20,36 @@ class SoulCaptureEnchantment : Enchantment(
 ) {
     override fun getMinLevel(): Int = 1
     override fun getMaxLevel(): Int = 1
+    override fun getMinPower(level: Int): Int {
+        return super.getMinPower(level)
+    }
+
+    fun handleEnchantment(world: ServerWorld?, attacker: Entity?, killed: LivingEntity?) {
+        if (shouldEventBeHandled(world, attacker, killed)) {
+            val soulCaptureLevel = EnchantmentHelper.getLevel(this, attacker.mainHandStack)
+            if (world.random.getBooleanWithPerMilleChance(soulCaptureLevel)) {
+                dropSpawnEgg(killed)
+            }
+        }
+    }
+
+    companion object {
+
+        fun Random.getBooleanWithPerMilleChance(perMille: Int): Boolean {
+            if (perMille < 0 || perMille > 1000) throw IllegalArgumentException()
+            if (perMille == 0) return false
+            return this.nextInt(1000) < perMille
+        }
+
+        fun dropSpawnEgg(killed: LivingEntity) = killed.dropItem {
+            SpawnEggItem.forEntity(killed.type)
+        }
+
+        @OptIn(ExperimentalContracts::class)
+        fun shouldEventBeHandled(world: ServerWorld?, attacker: Entity?, killed: LivingEntity?): Boolean {
+            contract { returns(true) implies (world != null && attacker != null && killed != null && attacker is PlayerEntity) }
+            return world != null && attacker != null && killed != null && attacker is PlayerEntity
+        }
+
+    }
 }
--- src/main/kotlin/de/bulldog98/soulcapture/SoulCaptureInitializer.kt
+++ src/main/kotlin/de/bulldog98/soulcapture/SoulCaptureInitializer.kt
@@ -1,42 +1,19 @@
 package de.bulldog98.soulcapture
 
-import de.bulldog98.event.api.KilledByCallback
 import net.fabricmc.api.ModInitializer
-import net.minecraft.enchantment.Enchantment
-import net.minecraft.enchantment.EnchantmentHelper
-import net.minecraft.entity.LivingEntity
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.ItemConvertible
-import net.minecraft.item.SpawnEggItem
-import net.minecraft.util.ActionResult
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
 class SoulCaptureInitializer : ModInitializer {
-    private val SOUL_CAPTURE: Enchantment = Registry.register(
+    private val SOUL_CAPTURE = Registry.register(
         Registry.ENCHANTMENT,
         Identifier("soulcapture", "soul_capture"),
         SoulCaptureEnchantment()
-    )
-
-    private fun handleSoulCapture(killedEntity: LivingEntity, adversary: LivingEntity?): ActionResult {
-        if (adversary != null && adversary is PlayerEntity) {
-            val soulCaptureLevel = EnchantmentHelper.getLevel(SOUL_CAPTURE, adversary.mainHandStack)
-            val randomNumber = killedEntity.entityWorld.random.nextInt(1001) - 1
-            if (randomNumber < soulCaptureLevel) {
-                killedEntity.dropItem(
-                    ItemConvertible {
-                        return@ItemConvertible SpawnEggItem.forEntity(killedEntity.type)
-                    }
-                )
-            }
-            return ActionResult.SUCCESS
-        }
-        return ActionResult.PASS
-    }
+    )!!
 
     override fun onInitialize() {
         println("soul capture initializing")
-        KilledByCallback.EVENT.register(this::handleSoulCapture)
+        ServerEntityCombatEvents.AFTER_KILLED_OTHER_ENTITY.register(SOUL_CAPTURE::handleEnchantment)
     }
 }
